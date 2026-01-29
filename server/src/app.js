d4e64f5dbf42120d504e57652a79b8f5d8e95cc4
tomasliivak <tomasliivak@gmail.com>
2026-01-28 19:12:01 -0500

    Fixed app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,26 @@
 import 'dotenv/config'
-import path from "path"
 import express from "express"
 import cors from "cors"
 import rateLimit from "express-rate-limit";
-
+import { pool } from "./db.js"
 import { receiptsRouter } from "./routes/receipts.js"
 
 
 const app = express()
 app.set("trust proxy", 1)
+/*change cors once I deploy vercel 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://YOUR_VERCEL_DOMAIN.vercel.app",
+];
+
+app.use(cors({
+  origin: (origin, cb) => {
+    if (!origin) return cb(null, true); // allows curl/postman
+    cb(null, allowedOrigins.includes(origin));
+  }
+}));
+*/
 app.use(cors())
 app.use(express.json({ limit: "20kb" }))
 
@@ -20,21 +32,15 @@ const generalLimiter = rateLimit({
 })
 
 app.use(generalLimiter)
-if (process.env.NODE_ENV === "production") {
-  const distPath = path.resolve("../client/dist")
-  app.use(express.static(distPath))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(distPath, "index.html"))
-  })
-}
-app.get("/health", async (req, res) => {
-  const r = await pool.query("SELECT 1 as ok");
-  res.json(r.rows[0]);
-})
 app.use((req, res, next) => {
   console.log("REQ:", req.method, req.url)
   next()
+})
+
+app.get("/health", async (req, res) => {
+  const r = await pool.query("SELECT 1 as ok");
+  res.json(r.rows[0]);
 })
 
 app.use("/api/receipts", receiptsRouter)
