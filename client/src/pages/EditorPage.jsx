b3e083b4a6eddcea36b904ca056d4251a8974caa
tomasliivak <tomasliivak@gmail.com>
2026-02-26 14:13:53 -0500

    Fixed toast import on editor page

--- client/src/pages/EditorPage.jsx
+++ client/src/pages/EditorPage.jsx
@@ -7,6 +7,8 @@ import "./ReceiptPage.css"
 import "./EditorPage.css"
 import posthog from "posthog-js"
 import Header from "../components/Header.jsx"
+import toast from "react-hot-toast"
+
 const API_URL = import.meta.env.PROD
   ? import.meta.env.VITE_API_URL
   : "http://localhost:3000";
